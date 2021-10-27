d84451e02a6f49103139efc2005ec442ae1a7f29
ZhengjunHUO <firelouiszj@hotmail.com>
2021-10-27 21:21:04 +0200

    add create file program

---
+++ primitive_io/createFile.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <unistd.h>
+#include <fcntl.h>
+#include <errno.h>
+#include <string.h>
+
+int main() {
+	//int fd = open("/tmp/helloworld", O_WRONLY | O_CREAT | O_TRUNC, 0664);
+	int fd = creat("/tmp/helloworld", 0644);
+	if (fd == -1) {
+		const int err = errno;
+		fprintf(stderr, "Create file failed: %s [%d]\n", strerror(err), err);
+		return EXIT_FAILURE;
+	}
+
+	printf("Create file [fd: %d] successed!\n", fd);
+	close(fd);
+	return EXIT_SUCCESS;
+}
