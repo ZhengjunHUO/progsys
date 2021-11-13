10e48609d6eb40147f7ffbd1de7e96b89ca8e1fd
ZhengjunHUO <firelouiszj@hotmail.com>
2021-11-13 18:35:16 +0100

    add wait test with normal exit

---
+++ process/wait_exit.c
@@ -0,0 +1,30 @@
+#include <sys/wait.h>
+#include <stdlib.h>
+#include <stdio.h>
+#include <unistd.h>
+
+int main() {
+	// Child process, exit with 5
+	if (!fork())
+		return 5;
+
+	// Parent process, wait and query the child's status
+	int status;
+	pid_t pid = wait(&status);
+	if (pid == -1) {
+		perror("wait");
+		return EXIT_FAILURE;
+	}
+
+	printf("Child[%d]: ", pid);
+	if (WIFEXITED(status))
+		printf("Normal exit with status %d\n", WEXITSTATUS(status));
+	if (WIFSIGNALED(status))
+		printf("Terminated by signal %d%s\n", WTERMSIG(status), WCOREDUMP(status) ? " [Core Dump]":"");
+	if (WIFSTOPPED(status))
+		printf("Stopped by signal %d\n", WSTOPSIG(status));
+	if (WIFCONTINUED(status))
+		printf("Continued\n");
+
+	return EXIT_SUCCESS;
+}
