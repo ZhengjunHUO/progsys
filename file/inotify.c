cf290fa0831fc5e68d756e2f973fbdee8810cd80
ZhengjunHUO <firelouiszj@hotmail.com>
2021-11-25 12:07:25 +0100

    add watch to inotify instance

--- file/inotify.c
+++ file/inotify.c
@@ -3,8 +3,10 @@
 #include <stdlib.h>
 #include <unistd.h>
 
+#define BUF_SIZE 4096
+
 int main() {
-	// init inotify instance to fd
+	// (1) init inotify instance to fd
 	int fd = inotify_init1(0);
 	if (fd == -1) {
 		perror("inotify_init1");
@@ -13,6 +15,21 @@ int main() {
 
 	printf("inotify instance created with fd[%d]\n", fd);
 
+	// (2) attach new watch to instance
+	int wd = inotify_add_watch(fd, "/tmp", IN_CREATE|IN_DELETE|IN_MODIFY);
+	if (wd == -1) {
+		perror("inotify_add_watch");
+		close(fd);
+		return EXIT_FAILURE;
+	}
+
+	printf("new watch with wd[%d] attach to inotify instance\n", wd);
+
+	// remove watch
+	if(inotify_rm_watch(fd, wd))
+		perror("inotify_rm_watch");
+
+	// close inotify instance
 	if(close(fd)) {
 		perror("close");
 		return EXIT_FAILURE;
