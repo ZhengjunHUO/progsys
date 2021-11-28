0a465edeb8f8ff7d664350f5b19d22c81c042baf
ZhengjunHUO <firelouiszj@hotmail.com>
2021-11-28 12:38:08 +0100

    improve calloc

--- ram/calloc.c
+++ ram/calloc.c
@@ -1,21 +1,24 @@
 #include <stdlib.h>
 #include <stdio.h>
+#include <malloc.h>
+
+#define REQUESTSIZE 25
 
 int main() {
-	char* c = calloc(16, sizeof(char));
+	char* c = calloc(REQUESTSIZE, sizeof(char));
 	if(!c) {
 		perror("calloc");
 		return EXIT_FAILURE;
 	}
 
-	char *m = malloc(16*sizeof(char));
+	char *m = malloc(REQUESTSIZE * sizeof(char));
 	if(!m) {
 		perror("malloc");
 		return EXIT_FAILURE;
 	}
 
-	printf("Calloc 16 bytes.\n");
-	printf("Malloc 16 bytes.\n");
+	printf("calloc %ld (request: %d) bytes.\n", malloc_usable_size(c), REQUESTSIZE);
+	printf("malloc %ld (request: %d) bytes.\n", malloc_usable_size(m), REQUESTSIZE);
 
 	free(c);
 	free(m);
