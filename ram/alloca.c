2ff35648f81a1b7b9abadd6e8d34fb47b125adbd
ZhengjunHUO <firelouiszj@hotmail.com>
2021-11-29 10:56:16 +0100

    alloca usecase malloc implement

--- ram/alloca.c
+++ ram/alloca.c
@@ -2,6 +2,9 @@
 #include <fcntl.h>
 #include <string.h>
 
+#include <stdio.h>
+#include <malloc.h>
+
 #define ETC_CONFIG_DIR "/etc/"
 
 int open_etc_config(const char *filename, int flags, mode_t mode) {
@@ -14,3 +17,23 @@ int open_etc_config(const char *filename, int flags, mode_t mode) {
 
 	return open(pathtofile, flags, mode);
 }
+
+int open_etc_config2(const char *filename, int flags, mode_t mode) {
+	const char *etc = ETC_CONFIG_DIR;
+
+	// allocate memory on the stack
+	char * pathtofile = malloc(strlen(etc) + strlen(filename) + 1);
+	if (!pathtofile) {
+		perror("malloc");
+		return -1;
+	}
+
+	strcpy(pathtofile, etc);
+	strcat(pathtofile, filename);
+
+	int fd = open(pathtofile, flags, mode);
+	free(pathtofile);
+
+	return fd;
+}
+
--- ram/malloc.h
+++ ram/malloc.h
@@ -2,3 +2,4 @@ extern void * xmalloc(size_t size);
 extern void * xmalloc0(size_t size);
 extern void printmallinfo();
 extern int open_etc_config(const char *filename, int flags, mode_t mode);
+extern int open_etc_config2(const char *filename, int flags, mode_t mode);
