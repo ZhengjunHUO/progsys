725518ba0642985dfbd8c9c52c89bdd217472397
ZhengjunHUO <firelouiszj@hotmail.com>
2021-12-08 11:04:31 +0100

    check timer

--- timedate/timer.c
+++ timedate/timer.c
@@ -26,9 +26,9 @@ int main() {
 
 	// arm a timer
 	struct itimerspec ts;
-	ts.it_interval.tv_sec = 3;
+	ts.it_interval.tv_sec = 1;
 	ts.it_interval.tv_nsec = 0;
-	ts.it_value.tv_sec = 1;
+	ts.it_value.tv_sec = 3;
 	ts.it_value.tv_nsec = 0;
 
 	if (timer_settime(t, 0, &ts, NULL)) {
@@ -40,6 +40,16 @@ int main() {
 	}
 
 	pause();
+
+	// Check alarm overrun
+	int ret = timer_getoverrun(t);
+	if (ret == -1) {
+		perror("timer_getoverrun");
+	}else if(ret == 0) {
+		printf("No overrun happened!\n");
+	}else{
+		printf("Alarm overrun %d sec!\n", ret);
+	}
 
 	// remove a timer
 	if (timer_delete(t)) {
